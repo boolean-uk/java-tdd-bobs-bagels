53b7620651d6d3aaaa8ee61b6617f523108ca4e9
Rose <rosevandergiessen@proton.me>
2023-05-10 14:30:02 +0200

    test succes addBagel added

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,14 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class Basket {
+    ArrayList<String> bagels;
+    int basketCapacity;
+
+    public Basket() {
+        this.bagels = new ArrayList<>();
+        this.basketCapacity = 3;
+    }
 
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -5,4 +5,10 @@ import org.junit.jupiter.api.Test;
 
 class BasketTest {
 
+    @Test
+    public void testAddBagel() {
+        // Test for successfully added Bagel
+        Basket basket = new Basket();
+        Assertions.assertTrue(basket.addBagel("Avocado"));
+    }
 }

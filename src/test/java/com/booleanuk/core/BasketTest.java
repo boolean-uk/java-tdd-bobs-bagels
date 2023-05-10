20827291f6828f4c7a8ed52f1ae49a0d5cac313f
Rose <rosevandergiessen@proton.me>
2023-05-10 15:02:52 +0200

    test failure addBagel added

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -11,5 +11,11 @@ class BasketTest {
         Basket basket = new Basket();
         Assertions.assertTrue(basket.addBagel("Avocado"));
         Assertions.assertTrue(basket.bagels.contains("Avocado"));
+
+        // Test for failure
+        basket.addBagel("Smoked Salmon");
+        basket.addBagel("Hummus");
+        Assertions.assertFalse(basket.addBagel("Grondboontjeboter"));
+        Assertions.assertFalse(basket.bagels.contains("Grondboontjeboter"));
     }
 }

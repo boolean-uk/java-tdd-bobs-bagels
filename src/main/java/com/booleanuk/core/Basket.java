47e8787385edc973ee01e209e94bbb55da2f3a78
Emil Solgaard <emil.solgaard@dk.experis.com>
2023-05-10 14:13:15 +0200

    Domain Model + Red Green Refactor tests created.

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,25 @@
 package com.booleanuk.core;
 
-public class Basket {
+import java.util.ArrayList;
 
+enum BASKETERROR {
+    BAGELNOTFOUND,
+    MAXCAPACITY
+}
+
+public class Basket {
+    ArrayList<String> bagels;
+    static int capacity = 1;
+    BASKETERROR lastError;
+
+    public Basket() {
+        this.bagels = new ArrayList<>(capacity);
+        this.lastError = null;
+    }
+
+    public boolean addBagel(String type) { return true; }
+
+    public boolean removeBagel(String type) { return true; }
+
+    static boolean setCapacity(int size) { return true; }
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -3,6 +3,77 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class BasketTest {
+public class BasketTest {
+    // addBagel() tests
+    @Test
+    void addBagelShouldSucceed() {
+        Basket basket = new Basket();
 
+        Assertions.assertEquals(1, Basket.capacity);
+
+        Assertions.assertTrue(basket.addBagel("A"));
+
+        Assertions.assertNotEquals(-1, basket.bagels.indexOf("A"));
+    }
+    @Test
+    void addBagelShouldFail() {
+        Basket basket = new Basket();
+        basket.addBagel("A");
+
+        Assertions.assertEquals(1, Basket.capacity);
+
+        Assertions.assertFalse(basket.addBagel("B"));
+
+        Assertions.assertEquals(-1, basket.bagels.indexOf("B"));
+
+        Assertions.assertEquals(BASKETERROR.MAXCAPACITY, basket.lastError);
+    }
+    // removeBagel() tests
+    @Test
+    void removeBagelShouldSucceed() {
+        Basket basket = new Basket();
+
+        Assertions.assertEquals(-1, basket.bagels.indexOf("A"));
+
+        basket.addBagel("A");
+
+        Assertions.assertNotEquals(-1, basket.bagels.indexOf("A"));
+
+        Assertions.assertTrue(basket.removeBagel("A"));
+
+        Assertions.assertEquals(-1, basket.bagels.indexOf("A"));
+    }
+    @Test
+    void removeBagelShouldFail() {
+        Basket basket = new Basket();
+
+        Assertions.assertEquals(-1, basket.bagels.indexOf("A"));
+
+        Assertions.assertFalse(basket.removeBagel("A"));
+
+        Assertions.assertEquals(-1, basket.bagels.indexOf("A"));
+
+        Assertions.assertEquals(BASKETERROR.BAGELNOTFOUND, basket.lastError);
+    }
+    // setCapacity() tests
+    @Test
+    void setCapacityShouldBe2() {
+        Basket basket = new Basket();
+
+        Assertions.assertEquals(1, Basket.capacity);
+
+        Assertions.assertTrue(Basket.setCapacity(2));
+
+        Assertions.assertEquals(2, Basket.capacity);
+    }
+    @Test
+    void setCapacityShouldNotBeMinus2() {
+        Basket basket = new Basket();
+
+        Assertions.assertEquals(1, Basket.capacity);
+
+        Assertions.assertFalse(Basket.setCapacity(-2));
+
+        Assertions.assertEquals(1, Basket.capacity);
+    }
 }

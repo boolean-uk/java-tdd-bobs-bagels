f4090803907b8338e5e2ffef2a86c712fa0ba77c
Anastasiya <ronas0709@gmail.com>
2023-07-11 10:51:42 +0200

    added testAddItem

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -3,6 +3,18 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class BasketTest {
+
+    @Test
+    public void testAddItem(){
+        Basket basket = new Basket(5);
+        assertEquals("Added item to basket, you can add 3 items more", basket.addItem("Bagel1",2));
+        assertEquals("Added item to basket, you can add 0 items more", basket.addItem("Bagel2",3));
+        assertEquals("Basket is full", basket.addItem("Bagel1",2));
+
+    }
 
 }

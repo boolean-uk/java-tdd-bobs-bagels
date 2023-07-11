c413fc5e3ecc42025b74cd35747d1ad032ef3e1d
Piotr Karolak <karolak.piotr.012@gmail.com>
2023-07-11 10:13:18 +0200

    shouldAddBagelToBasket test added

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,30 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class Basket {
+
+    private ArrayList<String> basket = new ArrayList<>();
+
+
+
+
+    public void add(String bagel) {
+
+    }
+
+    public ArrayList<String> getBasket() {
+        return basket;
+    }
+
+    /*
+    * 1.As a member of the public,
+So I can order a bagel before work,
+I'd like to add a specific type of bagel to my basket.
+2.
+As a member of the public,
+So I can change my order,
+I'd like to remove a bagel from my basket.
+*/
 
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -1,8 +1,30 @@
 package com.booleanuk.core;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class BasketTest {
+
+
+    Basket basket;
+
+    @BeforeEach
+    public void beforeStart() {
+        basket = new Basket();
+    }
+
+    @Test
+    public void shouldAddBagelToBasket() {
+        //when
+        String bagel = "Plain Bagel";
+        //given
+
+        basket.add(bagel);
+        //then
+
+        assertEquals(1, basket.getBasket().size());
+    }
 
 }

7a1db4214db0d1e9540e3811f9a180af70f4ad87
karim <01171252@pw.edu.pl>
2023-07-11 11:41:20 +0200

    add

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -13,18 +13,26 @@ public class Basket {
         this.bagels = new HashMap<>();
     }
 
-    public String add(String bagelType, int count) {
+    public String add(String bagelType, int count){
         if (!isOverfilled(count)) {
-            this.bagels.put(bagelType, count);
+            if (this.bagels.containsKey(bagelType)) {
+                this.bagels.replace(bagelType, this.bagels.get(bagelType) + count);
+            }else{
+                this.bagels.put(bagelType,  count);
+            }
             this.total += count;
             return "bagel added";
         }
         return "overfilled cart";
     }
 
-    public String add(String bagelType) {
+    public String add(String bagelType){
         if (!isOverfilled(1)) {
-            this.bagels.put(bagelType, 1);
+            if (this.bagels.containsKey(bagelType)) {
+                this.bagels.replace(bagelType, this.bagels.get(bagelType) + 1);
+            }else{
+                this.bagels.put(bagelType,  1);
+            }
             this.total += 1;
             return "bagel added";
         }

b29db934009f3861bda2b5acec407c631bc351d7
OliwiaKepczynska <oliwia.kepczynska@gmail.com>
2023-07-11 11:00:19 +0200

    adding new tests to BasketTest class

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -1,8 +1,60 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class BasketTest {
+import java.util.ArrayList;
 
+class BasketTest {
+    Basket basket;
+    private static String bagel;
+    private static String bagel2 = "bagel2";
+    private static String bagel3 = "bagel3";
+
+    private static ArrayList<String> bagels;
+
+    @BeforeAll
+    static void testSetup() {
+        bagel = "bagel1";
+        bagel2 = "bagel2";
+        bagel3 = "bagel1";
+        bagels.add(bagel);
+        bagels.add(bagel2);
+        bagels.add(bagel3);
+    }
+
+    @AfterEach
+    void resetTodoList() {
+        bagels.clear();
+    }
+
+    @Test
+    public void orderBagelTest() {
+        Assertions.assertTrue(basket.addBagel("bagel1"));
+    }
+
+    @Test
+    public void removeBagelTest() {
+        bagels.addBagel("bagel2");
+        bagels.removeBagel("bagel2");
+        Assertions.assertTrue(bagels.getAllBagels().isEmpty());
+    }
+
+    @Test
+    public void checkIsBasketFullWhenBasketIsFullTest() {
+        String bagel = "bagel4";
+        String bagel2 = "bagel4";
+        bagels.add(bagel);
+        bagels.add(bagel2);
+
+        Assertions.assertTrue(basket.isBasketFull(bagels));
+    }
+
+    @Test
+    public void checkIsBasketFullWhenBasketIsNotFullTest() {
+        Assertions.assertFalse(basket.isBasketFull(bagels));
+    }
 }
+

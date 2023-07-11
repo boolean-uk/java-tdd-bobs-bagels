a60dac5d60fbde8a7ba6fb62c8d48aba71268c73
MiloszSobczyk <m.sobczyk.official@gmail.com>
2023-07-11 10:26:04 +0200

    Added tests and method signatures

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,42 @@
 package com.booleanuk.core;
 
-public class Basket {
+import java.util.HashMap;
 
+public class Basket {
+    public HashMap<String, Integer> availableBagels;
+    public HashMap<String, Integer> bagelsCount;
+    public int capacity;
+    public int currentAmount;
+    Basket(int capacity) {
+        availableBagels = new HashMap<String, Integer>();
+        bagelsCount = new HashMap<String, Integer>();
+        this.capacity = capacity;
+        currentAmount = 0;
+        addDefaultBagelTypes();
+    }
+    private void addDefaultBagelTypes() {
+        availableBagels.put("Plain", 3);
+        availableBagels.put("Sourdough", 4);
+        availableBagels.put("Cinnamon", 5);
+        availableBagels.put("Everything", 6);
+    }
+    public boolean add(String bagelType, int amount) {
+        return true;
+    }
+    public boolean remove(String bagelType, int amount) {
+        return true;
+    }
+    public boolean changeCapacity(int newCapacity) {
+        capacity = newCapacity;
+        return true;
+    }
+    public int totalCost() {
+        return 0;
+    }
+    public boolean addBagelType(String bagelType, int cost) {
+        return true;
+    }
+    public boolean changeBagelPrice(String bagelType, int newCost) {
+        return true;
+    }
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -5,4 +5,62 @@ import org.junit.jupiter.api.Test;
 
 class BasketTest {
 
+    @Test
+    public void addTest() {
+        Basket basket = new Basket(5);
+        Assertions.assertTrue(basket.add("Plain", 1));
+        Assertions.assertFalse(basket.add("Everything", 8));
+        Assertions.assertFalse(basket.add("Plastic", 3));
+    }
+
+    @Test
+    public void removeTest() {
+        Basket basket = new Basket(10);
+        basket.add("Plain", 6);
+        basket.add("Everything", 2);
+        basket.add("Cinnamon", 2);
+        Assertions.assertTrue(basket.remove("Plain", 4));
+        Assertions.assertFalse(basket.remove("Everything", 4));
+        Assertions.assertFalse(basket.remove("Plastic", 2));
+    }
+
+    @Test
+    public void changeCapacityTest() {
+        Basket basket = new Basket(10);
+        basket.add("Plain", 6);
+        basket.add("Everything", 2);
+        basket.add("Cinnamon", 2);
+        Assertions.assertTrue(basket.changeCapacity(15));
+        Assertions.assertFalse(basket.changeCapacity(8));
+    }
+
+    @Test
+    public void totalCostTest() {
+        Basket basket = new Basket(10);
+        basket.add("Plain", 6);
+        basket.add("Everything", 2);
+        basket.add("Cinnamon", 2);
+        Assertions.assertEquals(40, basket.totalCost());
+    }
+
+    @Test
+    public void addBagelTypeTest() {
+        Basket basket = new Basket(10);
+        basket.add("Plain", 6);
+        basket.add("Everything", 2);
+        basket.add("Cinnamon", 2);
+        Assertions.assertTrue(basket.addBagelType("Blueberry", 4));
+        Assertions.assertFalse(basket.addBagelType("Strawberry", 0));
+        Assertions.assertFalse(basket.addBagelType("Plain", 4));
+    }
+    @Test
+    public void changeBagelPriceTest() {
+        Basket basket = new Basket(10);
+        basket.add("Plain", 6);
+        basket.add("Everything", 2);
+        basket.add("Cinnamon", 2);
+        Assertions.assertTrue(basket.changeBagelPrice("Plain", 7));
+        Assertions.assertFalse(basket.changeBagelPrice("Everything", 0));
+        Assertions.assertFalse(basket.changeBagelPrice("Blueberry", 6));
+    }
 }

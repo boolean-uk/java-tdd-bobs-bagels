b59ee97b17546669901ea8df55ff8183b104772c
Kinga <kinga.hernet@talan.com>
2023-07-11 10:00:15 +0200

    Added testAddBagled

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -4,5 +4,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BasketTest {
+    @Test
+    public void testAddBagel() {
+        Basket basket = new Basket();
+        Bagel plainBagel = new Bagel("plain");
+
+        basket.addBagel(plainBagel);
+
+        assertEquals(1, basket.getSize());
+        assertTrue(basket.containsBagel(plainBagel));
+    }
+
+
 
 }

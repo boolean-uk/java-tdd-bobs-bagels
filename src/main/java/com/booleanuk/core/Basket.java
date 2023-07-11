9c71b78b18f030b199bd0fd1ceda287fa87d1378
Martyna <martyna.marszalek@talan.com>
2023-07-11 10:24:55 +0200

    Implementation for function add

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,17 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Basket {
 
+    List<String> bagels = new ArrayList<>();
+
+    public List<String> getBagels() {
+        return bagels;
+    }
+
+    public void add(String bagel){
+        bagels.add(bagel);
+    }
 }

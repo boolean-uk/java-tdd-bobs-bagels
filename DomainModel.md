ff3aac7a4d05b00d5361340300bb628427058e7c
Julia Girejko <julia.girejko@talan.com>
2023-07-11 10:32:28 +0200

    test commit

--- DomainModel.md
+++ DomainModel.md
@@ -1,15 +1,15 @@
 Domain model:
 
 
-| Classes  | Methods         | Scenario                       | Outputs  |
-|----------|-----------------|--------------------------------|----------|
-| `Basket` | `addBagel()`    | basket is not full             | True     |
-|          |                 | basket is full                 | False    |
-|          | `removeBagel()` | bagel is not the basket yet    | False    |
-|          |                 | bagel is in the basket         | True     |
-|          | `basketCheck()` | basket is not full             | int size |
-|          |                 | basket is full                 | 0        |
-|          | `sizeChange()`  | new basket size is not too big | True     |
-|          |                 | new basket is too big          | False    |
-|          | `bagelCheck()`  | bagel is not the basket yet    | False    |
-|          |                 | bagel is in the basket         | True     |
+| Classes  | Methods                | Scenario                               | Outputs  |
+|----------|------------------------|----------------------------------------|----------|
+| `Basket` | `addBagel()`           | basket is not full                     | True     |
+|          |                        | basket is full                         | False    |
+|          | `removeBagel()`        | bagel is not the basket yet            | False    |
+|          |                        | bagel is in the basket                 | True     |
+|          | `basketCheck()`        | basket is not full                     | int size |
+|          |                        | basket is full                         | 0        |
+|          | `sizeChange(int size)` | new basket size is not bigger than 100 | True     |
+|          |                        | new basket is bigger than 100          | False    |
+|          | `bagelCheck()`         | bagel is not the basket yet            | False    |
+|          |                        | bagel is in the basket                 | True     |
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -5,4 +5,67 @@ import org.junit.jupiter.api.Test;
 
 class BasketTest {
 
+    @Test
+    public void basketCheckTest() {
+        Basket basket1 = new Basket();
+        Basket basket2 = new Basket();
+        Basket basket3 = new Basket();
+        Basket basket4 = new Basket();
+        Basket[] basketArray = new Basket[3];
+        basketArray[0] = basket1;
+        basketArray[1] = basket2;
+        boolean boolCheckTrue = basket4.basketCheck();
+        Assertions.assertTrue(boolCheckTrue);
+        basketArray[2] = basket3;
+        boolean boolCheckFalse = basket4.basketCheck();
+        Assertions.assertFalse(boolCheckFalse);
+    }
+
+    @Test
+    public void bagelCheckTest() {
+        Basket basket1 = new Basket();
+        Basket basket2 = new Basket();
+        Basket basket3 = new Basket();
+        Basket[] basketArray = new Basket[3];
+        basketArray[0] = basket1;
+        basketArray[1] = basket2;
+        boolean boolCheckFalse = basket3.bagelCheck();
+        Assertions.assertFalse(boolCheckFalse);
+        boolean boolCheckTrue = basket2.bagelCheck();
+        Assertions.assertTrue(boolCheckTrue);
+    }
+
+    @Test
+    public void removeBagelTest() {
+        Basket basket1 = new Basket();
+        Basket basket2 = new Basket();
+        Basket basket3 = new Basket();
+        Basket[] basketArray = new Basket[3];
+        basketArray[0] = basket1;
+        basketArray[1] = basket2;
+        boolean boolCheckFalse = basket3.removeBagel();
+        Assertions.assertFalse(boolCheckFalse);
+        boolean boolCheckTrue = basket2.removeBagel();
+        Assertions.assertTrue(boolCheckTrue);
+    }
+
+    @Test
+    public void addBagelTest() {
+        Basket basket1 = new Basket();
+        Basket basket2 = new Basket();
+        Basket[] basketArray = new Basket[1];
+        boolean boolCheckTrue = basket1.addBagel();
+        Assertions.assertTrue(boolCheckTrue);
+        boolean boolCheckFalse = basket2.addBagel();
+        Assertions.assertFalse(boolCheckFalse);
+    }
+
+    @Test
+    public void sizeChangeTest() {
+        Basket[] basketArray = new Basket[1];
+        boolean boolCheckTrue = basketArray.sizeChange(30);
+        Assertions.assertTrue(boolCheckTrue);
+        boolean boolCheckFalse = basketArray.sizeChange(101);
+        Assertions.assertFalse(boolCheckFalse);
+    }
 }

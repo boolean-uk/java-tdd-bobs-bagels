0bc717e0fa666bce9056564fcf6d2e46d373f1e2
Przemyslaw Mroczek <przemyslaw.mroczek@talan.com>
2023-07-11 10:34:14 +0200

    User Story 1 - red test

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -1,8 +1,19 @@
 package com.booleanuk.core;
 
+import com.booleanuk.core.Basket.Bagel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BasketTest {
 
+    @Test
+    public void shouldAddBagelToBasket() {
+        Basket basket = new Basket();
+        Bagel bagel = new Bagel(Basket.BagelType.CLASSIC);
+
+        basket.add(bagel);
+
+        Assertions.assertEquals(1, basket.bagels.size());
+
+    }
 }

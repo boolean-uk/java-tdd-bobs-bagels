0b1ba1ef1794dd7ce7ff2e879b6b1b763234df56
Marcin Stefanowicz <marcin.stefanowicz@talan.com>
2023-07-11 11:18:53 +0200

    red stage of testAdd_WhenThereIsEnoughSpaceInTheBasket_ShouldReturnTrue

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -4,5 +4,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BasketTest {
+    @Test
+    public void testAdd_WhenThereIsEnoughSpaceInTheBasket_ShouldReturnTrue(){
+        //Given
+        Basket basket = new Basket();
 
+        //When
+        boolean result = basket.add("chocolate bagel", 2);
+
+        //Then
+        Assertions.assertTrue(result);
+    }
 }

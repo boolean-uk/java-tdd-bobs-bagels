8aa1f80ae57262c7bd03222d6225a36aa83c562d
Przemyslaw Mroczek <przemyslaw.mroczek@talan.com>
2023-07-11 11:07:14 +0200

    User Story 3 - red test

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -4,7 +4,9 @@ import com.booleanuk.core.Basket.Bagel;
 import com.booleanuk.core.Basket.BagelType;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BasketTest {
 
@@ -28,5 +30,14 @@ class BasketTest {
         assertTrue(basket.remove(bagel.id));
         assertFalse(basket.remove(bagel.id));
         assertEquals(0, basket.bagels.size());
+    }
+
+    @Test
+    public void shouldPreventOverfillingBasket() {
+        Basket basket = new Basket(2);
+        basket.add(new Bagel(BagelType.CLASSIC));
+
+        assertTrue(basket.add(new Bagel(BagelType.CLASSIC)));
+        assertFalse(basket.add(new Bagel(BagelType.CLASSIC)));
     }
 }

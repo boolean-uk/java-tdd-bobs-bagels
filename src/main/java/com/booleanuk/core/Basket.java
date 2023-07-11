1abdc00552854e0d3ccfad1853cb65944eb9bd25
Anna Sperkach <annsperkach@gmail.com>
2023-07-11 11:49:06 +0300

    Add and remove methods

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -3,5 +3,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Basket {
+    private int capacity;
+    private List<String> items;
 
+    public Basket(int capacity) {
+        this.capacity = capacity;
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(String item) {
+        if (items.size() >= capacity) {
+            throw new RuntimeException("Basket is full");
+        }
+        items.add(item);
+    }
+
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public void removeItem(String item) {
+        if (!items.contains(item)) {
+            throw new RuntimeException("Item isn't found in the basket");
+        }
+        items.remove(item);
+    }
 }

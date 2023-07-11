3992e069642dca5e43faf869d1da41eca46a2263
Adam Ciesielski <ciesielski.adam2002@gmail.com>
2023-07-11 10:09:10 +0200

    added "add" test

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,14 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class Basket {
+
+    ArrayList<String> bagelsBasket=new ArrayList<>();
+
+    public void add(String bagel)
+    {
+
+    }
 
 }

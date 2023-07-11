23fb0b3d3667660b7929c499ad1d7da747383590
Ilias Belghoul <ilias.belghoul@talan.com>
2023-07-11 10:38:00 +0200

    add addBagel method and tests

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,28 @@
 package com.booleanuk.core;
 
-public class Basket {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Basket {
+    private final int capacity;
+    private final List<String> bagels;
+
+    public void addBagel(String bagel) {
+        if (bagels.size() < capacity) {
+            bagels.add(bagel);
+        }
+    }
+
+    public Basket(int capacity) {
+        this.capacity = capacity;
+        this.bagels = new ArrayList<>();
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public List<String> getBagels() {
+        return bagels;
+    }
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -1,8 +1,42 @@
 package com.booleanuk.core;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BasketTest {
+    private Basket basket;
+    @BeforeEach
+    void setup() {
+        basket = new Basket(3);
+    }
+    @Test
+    public void shouldAddBagelToBasket() {
+        //Setup
+        String bagel = "Plain bagel";
 
+        //Execute
+        basket.addBagel(bagel);
+
+        //Verify
+        Assertions.assertTrue(basket.getBagels().contains(bagel));
+    }
+
+    @Test
+    public void shouldNotAddBagelToBasketWhenBasketIsFull() {
+        //Setup
+        String bagel1 = "Plain bagel";
+        String bagel2 = "Plain bagel";
+        String bagel3 = "Plain bagel";
+        basket.addBagel(bagel1);
+        basket.addBagel(bagel2);
+        basket.addBagel(bagel3);
+        String bagel4 = "Egg bagel";
+
+        //Execute
+        basket.addBagel(bagel4);
+
+        //Verify
+        Assertions.assertFalse(basket.getBagels().contains(bagel4));
+    }
 }

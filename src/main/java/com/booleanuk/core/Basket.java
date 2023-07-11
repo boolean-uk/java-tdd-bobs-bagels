7a7cb69f2d78506f9405341564153e41dd12e926
PawelCagara <pawel.cagara@talan.com>
2023-07-11 11:51:41 +0200

    changeCapacityOfBasket() method updated

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -33,12 +33,16 @@ public class Basket {
     }
 
     public void changeCapacityOfBasket(int newSize) {
-        if (newSize > 0 && newSize <= bagles.size())
+        if (newSize > 0 && bagles.size() == 0)
             this.capacity = newSize;
         else if (newSize <= 0)
             System.out.println("New capacity size must be positive. You cannot set it to " + newSize + ".");
-        else if(newSize >= bagles.size())
-            System.out.println("You already got " + bagles.size() + " bagles in basket so you can't downsize basket to " + newSize + " spaces");
+        else if(newSize >= bagles.size()){
+            System.out.println("You already got " + bagles.size() + " bagles in basket so you can't downsize basket to " + newSize + " spaces.");
+            System.out.println("New basket size is " + bagles.size());
+            this.capacity = bagles.size();
+        }
+
 
     }
 

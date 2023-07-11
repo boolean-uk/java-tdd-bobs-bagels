b26fbc801a0c2895cfcd7ae7e121ffaae0f0af9f
Rafal Halama <rafau.halama@gmail.com>
2023-07-11 09:54:50 +0200

    Add method implemented

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,24 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Basket {
 
+    List<String> bagles;
+    int capacity;
+    public Basket(int capacity){
+        bagles = new ArrayList<>();
+        this.capacity = capacity;
+    }
+
+    public boolean add(String type) {
+        if(bagles.size()< capacity){
+            bagles.add(type);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }

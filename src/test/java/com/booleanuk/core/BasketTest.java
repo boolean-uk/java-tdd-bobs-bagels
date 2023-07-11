d11e951f68d74b6615eabcb1ee081e156be0279d
Michal Antoniuk <michal.antoniuk@talan.com>
2023-07-11 10:41:30 +0200

    User Story 2 - red test

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -1,19 +1,32 @@
 package com.booleanuk.core;
 
 import com.booleanuk.core.Basket.Bagel;
+import com.booleanuk.core.Basket.BagelType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BasketTest {
 
     @Test
     public void shouldAddBagelToBasket() {
         Basket basket = new Basket();
-        Bagel bagel = new Bagel(Basket.BagelType.CLASSIC);
+        Bagel bagel = new Bagel(BagelType.CLASSIC);
 
         basket.add(bagel);
 
-        Assertions.assertEquals(1, basket.bagels.size());
+        assertEquals(1, basket.bagels.size());
 
+    }
+
+    @Test
+    public void shouldRemoveBagelFromBasket() {
+        Basket basket = new Basket();
+        Bagel bagel = new Bagel(BagelType.CLASSIC);
+        basket.add(bagel);
+        basket.remove(bagel.id);
+
+        assertEquals(0, basket.bagels.size());
     }
 }

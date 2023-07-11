2cebf92f565e517e83e2089f9277ec947872aab0
Martyna <martyna.marszalek@talan.com>
2023-07-11 10:21:49 +0200

    Test for function add

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -1,8 +1,27 @@
 package com.booleanuk.core;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BasketTest {
+
+    Basket basket;
+
+    @BeforeEach
+    void setUp(){
+        basket = new Basket();
+    }
+
+    @Test
+    public void testAdd(){
+        basket.add("First bagel");
+        Assertions.assertEquals(1, basket.getBagels().size());
+        Assertions.assertTrue(basket.getBagels().contains("First bagel"));
+    }
+
+
+
+
 
 }

5cb18710523b0a7840937f3c3a627acaa16ad77f
Ilias Belghoul <ilias.belghoul@talan.com>
2023-07-11 11:09:13 +0200

    add isBasketFull method and tests

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -7,12 +7,6 @@ public class Basket {
     private final int capacity;
     private final List<String> bagels;
 
-    public void addBagel(String bagel) {
-        if (bagels.size() < capacity) {
-            bagels.add(bagel);
-        }
-    }
-
     public Basket(int capacity) {
         this.capacity = capacity;
         this.bagels = new ArrayList<>();
@@ -24,5 +18,23 @@ public class Basket {
 
     public List<String> getBagels() {
         return bagels;
+    }
+
+    public void removeBagel(String bagel2) {
+        int index = bagels.lastIndexOf(bagel2);
+        if (index != -1) {
+            bagels.remove(index);
+        }
+    }
+
+    public boolean isBasketFull() {
+        return bagels.size() == capacity;
+    }
+    public void addBagel(String bagel) {
+        if (!isBasketFull()) {
+            bagels.add(bagel);
+        } else {
+            System.out.println("Basket is full");
+        }
     }
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -4,16 +4,21 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 class BasketTest {
     private Basket basket;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
     @BeforeEach
     void setup() {
         basket = new Basket(3);
+        System.setOut(new PrintStream(outputStreamCaptor));
     }
     @Test
     public void shouldAddBagelToBasket() {
         //Setup
-        String bagel = "Plain bagel";
+        String bagel = "Plain";
 
         //Execute
         basket.addBagel(bagel);
@@ -25,18 +30,87 @@ class BasketTest {
     @Test
     public void shouldNotAddBagelToBasketWhenBasketIsFull() {
         //Setup
-        String bagel1 = "Plain bagel";
-        String bagel2 = "Plain bagel";
-        String bagel3 = "Plain bagel";
+        String bagel1 = "Plain";
+        String bagel2 = "Plain";
+        String bagel3 = "Plain";
         basket.addBagel(bagel1);
         basket.addBagel(bagel2);
         basket.addBagel(bagel3);
-        String bagel4 = "Egg bagel";
+        String bagel4 = "Egg";
 
         //Execute
         basket.addBagel(bagel4);
 
         //Verify
         Assertions.assertFalse(basket.getBagels().contains(bagel4));
+    }
+
+    @Test
+    public void shouldRemoveBagelFromBasket() {
+        //Setup
+        String bagel = "Plain";
+        String bagel2 = "Sesame";
+        String bagel3 = "Plain";
+        basket.addBagel(bagel);
+        basket.addBagel(bagel2);
+        basket.addBagel(bagel3);
+
+        //Execute
+        basket.removeBagel(bagel3);
+
+        //Verify
+        Assertions.assertEquals(2,basket.getBagels().size());
+    }
+
+    @Test
+    public void shouldPrintInfoThatBasketIsFull() {
+        //Setup
+        String bagel = "Plain";
+        String bagel2 = "Sesame";
+        String bagel3 = "Plain";
+        String bagel4 = "Egg";
+        basket.addBagel(bagel);
+        basket.addBagel(bagel2);
+        basket.addBagel(bagel3);
+
+        //Execute
+        basket.addBagel(bagel4);
+
+        //Verify
+        Assertions.assertEquals("Basket is full", outputStreamCaptor.toString()
+                .trim());
+    }
+
+    @Test
+    public void shouldReturnFalseIfBasketIsNotFull() {
+        //Setup
+        String bagel = "Plain";
+        String bagel2 = "Sesame";
+        basket.addBagel(bagel);
+        basket.addBagel(bagel2);
+
+        //Execute
+        boolean isBasketFull = basket.isBasketFull();
+
+        //Verify
+        Assertions.assertFalse(isBasketFull);
+    }
+
+    @Test
+    public void shouldReturnTrueIfBasketIsFull() {
+        //Setup
+        String bagel = "Plain";
+        String bagel2 = "Sesame";
+        String bagel3 = "Plain";
+
+        basket.addBagel(bagel);
+        basket.addBagel(bagel2);
+        basket.addBagel(bagel3);
+
+        //Execute
+        boolean isBasketFull = basket.isBasketFull();
+
+        //Verify
+        Assertions.assertTrue(isBasketFull);
     }
 }

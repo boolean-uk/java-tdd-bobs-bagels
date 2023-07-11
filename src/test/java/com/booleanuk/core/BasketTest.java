cdde4e97c207f387c5919d1487d1d6ff18719922
Szkaradd <szkaradekmikolaj@gmail.com>
2023-07-11 10:45:23 +0200

    added tests for removeFromBasket

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -4,14 +4,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Arrays;
+import java.util.List;
 
 class BasketTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
     private Basket basket;
     private static final int INITIAL_CAPACITY = 3;
     private String message;
     @BeforeEach
     public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
         basket = new Basket(INITIAL_CAPACITY);
     }
 
@@ -23,7 +29,7 @@ class BasketTest {
         basket.addToBasket(bagelType);
 
         // Verify
-        Assertions.assertEquals(Arrays.asList(bagelType), basket.getContents());
+        Assertions.assertEquals(List.of(bagelType), basket.getContents());
     }
 
     @Test
@@ -40,5 +46,35 @@ class BasketTest {
 
         // Verify
         Assertions.assertEquals("java.lang.Exception: Bagel Type does not exist", message);
+    }
+
+    @Test
+    public void shouldRemoveFromBasket() throws Exception {
+        // Setup
+        String bagelType1 = "Ham and cheese";
+        String bagelType2 = "Cheese and bacon";
+        basket.addToBasket(bagelType1);
+        basket.addToBasket(bagelType2);
+
+        // Execute
+        basket.removeFromBasket("Ham and cheese");
+
+        // Verify
+        Assertions.assertEquals(List.of("Cheese and bacon"), basket.getContents());
+    }
+
+    @Test
+    public void shouldShowInfoThatBagelIsNotPresent() throws Exception{
+        // Setup
+        String bagelType1 = "Ham and cheese";
+        basket.addToBasket(bagelType1);
+
+        // Execute
+        basket.removeFromBasket("Cheese and bacon");
+
+        // Verify
+        String expected = "Cheese and bacon bagel is not in the basket";
+        Assertions.assertEquals(expected, outputStreamCaptor.toString()
+                .trim());
     }
 }

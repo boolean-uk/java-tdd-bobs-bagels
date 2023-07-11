0aead3a14d4954fe18a908fd1394e910c5942c58
Anastasiya <ronas0709@gmail.com>
2023-07-11 10:52:13 +0200

    added Basket addItem

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,26 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+
 public class Basket {
+    private HashMap<String, Integer> items;
+    private int capacity;
+    private int currentLoad;
+    public Basket(int capacity){
+        this.items = new HashMap<>();
+        this.capacity = capacity;
+        this.currentLoad = 0;
+
+    }
+
+    public String addItem(String item, int quantity){
+
+        if (capacity-currentLoad>=quantity){
+            currentLoad+=quantity;
+            items.put(item,quantity);
+            return "Added item to basket, you can add " + String.valueOf(capacity-currentLoad) + " items more";
+        }
+        return "Basket is full";
+    }
 
 }

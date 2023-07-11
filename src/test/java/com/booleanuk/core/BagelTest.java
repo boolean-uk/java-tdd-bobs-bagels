21f82b7a2a2d652872a4e30fdc305df367d742b6
Kinga <kinga.hernet@talan.com>
2023-07-11 10:09:20 +0200

    Added testAddBeyondCapacity

---
+++ src/test/java/com/booleanuk/core/BagelTest.java
@@ -0,0 +1,35 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BagelTest {
+    @Test
+    public void testAddBagel() {
+        Basket basket = new Basket();
+        Bagel plainBagel = new Bagel("plain");
+
+        basket.addBagel(plainBagel);
+
+        Assertions.assertEquals(1, basket.getSize());
+        Assertions.assertTrue(basket.containsBagel(plainBagel));
+    }
+
+    @Test(expected = BasketFullException.class)
+    public void testAddBeyondCapacity() {
+        // Given
+        Basket basket = new Basket(2); // Assuming capacity is set in constructor
+        Bagel plainBagel = new Bagel("plain");
+        Bagel sesameBagel = new Bagel("sesame");
+        Bagel poppyBagel = new Bagel("poppy");
+
+        // When
+        basket.addBagel(plainBagel);
+        basket.addBagel(sesameBagel);
+        basket.addBagel(poppyBagel); // Should throw exception
+    }
+
+
+
+
+}
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -4,15 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BasketTest {
-    @Test
-    public void testAddBagel() {
-        Basket basket = new Basket();
-        Bagel plainBagel = new Bagel("plain");
 
-        basket.addBagel(plainBagel);
 
-        Assertions.assertEquals(1, basket.getSize());
-        Assertions.assertTrue(basket.containsBagel(plainBagel));
-    }
+
 
 }

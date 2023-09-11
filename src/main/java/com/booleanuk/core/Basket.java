684416cabc5cb3ca10301142b66ecd1314bca903
iliask796 <iliaskal7@gmail.com>
2023-09-11 17:34:15 +0300

    First Test Fail

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,21 @@
 package com.booleanuk.core;
 
-public class Basket {
+import java.util.ArrayList;
 
+public class Basket {
+    ArrayList<String> contents;
+
+    public Basket() {
+        this.contents = new ArrayList<>(){
+            {
+                add("Plain");
+                add("CinnamonRaisin");
+                add("PoppySeed");
+            }
+        };
+    }
+
+    public void add(String bagel){
+
+    }
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -4,5 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BasketTest {
-
+    @Test
+    public void simpleAddShouldAlwaysSucceed() {
+        Basket bagels = new Basket();
+        Assertions.assertTrue(bagels.contents.contains("Plain"));
+        bagels.add("Blueberry");
+        Assertions.assertTrue(bagels.contents.contains("Blueberry"));
+    }
 }

9b69a66d44f2ace79af2b75d296cb915570f7cbd
Ali Keskin <cmdalikeskin@gmail.com>
2023-09-12 01:04:11 +0200

    test added for add method

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -5,7 +5,30 @@ import org.junit.jupiter.api.Test;
 
 class BasketTest {
 
+    @Test
+    void addProductsChecker() {
+        Basket basket = new Basket();
+        String productOne = "Plain";
+        String productTwo = "Cinnamon";
+        String productThree = "Blueberry";
+        String productFour = "Poppy-seed";
+        String productFive = "Onion";
+        String productSix = "Garlic";
 
+
+        basket.add(productOne);
+        basket.add(productTwo);
+        basket.add(productThree);
+        Assertions.assertTrue(basket.contains(productOne));
+        Assertions.assertTrue(basket.contains(productTwo));
+        Assertions.assertTrue(basket.contains(productThree));
+
+        Assertions.assertFalse(basket.contains(productFour));
+        Assertions.assertFalse(basket.contains(productFive));
+        Assertions.assertFalse(basket.contains(productSix));
+
+
+    }
 
 
 }

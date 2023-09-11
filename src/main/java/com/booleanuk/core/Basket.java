f965c627bde3e5744fd7078efdb938ff356cc581
VeroniqueDM <veronika.dmiteva@gmail.com>
2023-09-11 21:10:39 +0200

    added one more test and wrote the add method

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,30 @@
 package com.booleanuk.core;
 
-public class Basket {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
+public class Basket {
+    public ArrayList<String> inventory;
+    public ArrayList<String> basket;
+    public int capacity;
+
+    public Basket(int capacity) {
+        this.inventory = new ArrayList<>(Arrays.asList("Sourdough",
+                "Blueberry", "Apple", "Cinnamon Raisin",
+                "Chocolate Chip", "Onion"));
+        this.basket = new ArrayList<>();
+        this.capacity = capacity;
+    }
+
+    public boolean add(String product) {
+        if (basket.contains(product)) {
+            return false;
+        }
+        if (this.inventory.contains(product) && this.basket.size() < this.capacity) {
+            this.basket.add(product);
+            return true;
+        }
+        return false;
+    }
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -6,17 +6,24 @@ import org.junit.jupiter.api.Test;
 class BasketTest {
     @Test
     public void testAddExistingProductToBasket() {
-        Basket basket = new Basket();
-        Assertions.assertTrue(basket.add("Apple", 2));
-        Assertions.assertEquals(1, basket.items.size());
-        Assertions.assertTrue(basket.items.containsKey("Apple"));
-        Assertions.assertEquals(2, basket.items.get("Apple").intValue());
+        Basket basket = new Basket(5);
+        Assertions.assertTrue(basket.add("Apple"));
+        Assertions.assertEquals(1, basket.basket.size());
+        Assertions.assertTrue(basket.basket.contains("Apple"));
     }
     @Test
     public void testAddNonExistentProductToBasket() {
-        Basket basket = new Basket();
-        Assertions.assertTrue(basket.add("Garlic", 1));
-        Assertions.assertEquals(0, basket.items.size());
+        Basket basket = new Basket(5);
+        Assertions.assertTrue(basket.add("Garlic"));
+        Assertions.assertEquals(0, basket.basket.size());
     }
 
+    @Test
+    public void testAddProductThatIsAlreadyInBasket() {
+        Basket basket = new Basket(5);
+        Assertions.assertTrue(basket.add("Apple"));
+        Assertions.assertEquals(1, basket.basket.size());
+        Assertions.assertFalse(basket.add("Apple"));
+        Assertions.assertEquals(1, basket.basket.size());
+    }
 }

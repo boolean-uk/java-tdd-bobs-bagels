28d7054a358c7a3d11783b59d6fbd703ade25fb9
Esther <estherbuijnsters@gmail.com>
2023-09-11 19:04:13 +0200

    Fourth Test (add bagel) passed

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -9,6 +9,14 @@ public class Basket {
     public boolean search(List<String> bagelInventory, String bagelType) {
         return bagelInventory.contains(bagelType);
     }
+
+    public boolean addToBagelBasket(List<String> BagelBasket, String bagelType) {
+        if (!BagelBasket.contains(bagelType)) {
+            BagelBasket.add(bagelType);
+            return true;
+        }
+        return false;
+    }
 }
 
 

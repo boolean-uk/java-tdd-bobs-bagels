8d1f95f99dbcf93c5b7507d2b40558f0d34bc148
VeroniqueDM <veronika.dmiteva@gmail.com>
2023-09-11 21:25:37 +0200

    added tests for hecking capacity

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -2,29 +2,32 @@ package com.booleanuk.core;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 
 public class Basket {
     public ArrayList<String> inventory;
-    public ArrayList<String> basket;
+    public ArrayList<String> bagelsBasket;
     public int capacity;
 
     public Basket(int capacity) {
         this.inventory = new ArrayList<>(Arrays.asList("Sourdough",
                 "Blueberry", "Apple", "Cinnamon Raisin",
                 "Chocolate Chip", "Onion"));
-        this.basket = new ArrayList<>();
+        this.bagelsBasket = new ArrayList<>();
         this.capacity = capacity;
     }
 
     public boolean add(String product) {
-        if (basket.contains(product)) {
+        if (this.bagelsBasket.contains(product)) {
             return false;
         }
-        if (this.inventory.contains(product) && this.basket.size() < this.capacity) {
-            this.basket.add(product);
+        if (this.inventory.contains(product) && this.bagelsBasket.size() < this.capacity) {
+            this.bagelsBasket.add(product);
             return true;
         }
         return false;
+    }
+
+    public boolean remove(String product) {
+        return this.bagelsBasket.remove(product);
     }
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -8,39 +8,51 @@ class BasketTest {
     public void testAddExistingProductToBasket() {
         Basket basket = new Basket(5);
         Assertions.assertTrue(basket.add("Apple"));
-        Assertions.assertEquals(1, basket.basket.size());
-        Assertions.assertTrue(basket.basket.contains("Apple"));
+        Assertions.assertEquals(1, basket.bagelsBasket.size());
+        Assertions.assertTrue(basket.bagelsBasket.contains("Apple"));
     }
     @Test
     public void testAddNonExistentProductToBasket() {
         Basket basket = new Basket(5);
         Assertions.assertTrue(basket.add("Garlic"));
-        Assertions.assertEquals(0, basket.basket.size());
+        Assertions.assertEquals(0, basket.bagelsBasket.size());
     }
 
     @Test
     public void testAddProductThatIsAlreadyInBasket() {
         Basket basket = new Basket(5);
         Assertions.assertTrue(basket.add("Apple"));
-        Assertions.assertEquals(1, basket.basket.size());
+        Assertions.assertEquals(1, basket.bagelsBasket.size());
         Assertions.assertFalse(basket.add("Apple"));
-        Assertions.assertEquals(1, basket.basket.size());
+        Assertions.assertEquals(1, basket.bagelsBasket.size());
     }
 
     @Test
     public void testRemoveExistingProductToBasket() {
         Basket basket = new Basket(5);
         Assertions.assertTrue(basket.add("Apple"));
-        Assertions.assertEquals(1, basket.basket.size());
-        Assertions.assertTrue(basket.basket.remove("Apple"));
-        Assertions.assertEquals(0, basket.basket.size());
+        Assertions.assertEquals(1, basket.bagelsBasket.size());
+        Assertions.assertTrue(basket.bagelsBasket.remove("Apple"));
+        Assertions.assertEquals(0, basket.bagelsBasket.size());
     }
 
     @Test
     public void testRemoveNonExistingProductToBasket() {
         Basket basket = new Basket(5);
         Assertions.assertTrue(basket.add("Apple"));
-        Assertions.assertEquals(1, basket.basket.size());
-        Assertions.assertFalse(basket.basket.remove("Onion"));
+        Assertions.assertEquals(1, basket.bagelsBasket.size());
+        Assertions.assertFalse(basket.bagelsBasket.remove("Onion"));
     }
+    @Test
+    public void testIsFullWhenCapacityIsAtMax() {
+        Basket basket = new Basket(3);
+        Assertions.assertTrue(basket.add("Apple"));
+        Assertions.assertTrue(basket.add("Onion"));
+        Assertions.assertFalse(basket.isFull());
+        Assertions.assertTrue(basket.add("Blueberry"));
+        Assertions.assertFalse(basket.isFull());
+        Assertions.assertEquals(3, basket.bagelsBasket.size());
+        Assertions.assertTrue(basket.isFull());
+    }
+
 }

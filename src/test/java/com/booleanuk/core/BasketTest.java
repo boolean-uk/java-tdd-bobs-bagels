0436c3a25e7bc70b0ae6f1bcc29266dd5840434e
Esther <estherbuijnsters@gmail.com>
2023-09-11 17:31:49 +0200

    second Test failed

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -3,12 +3,26 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class BasketTest {
 
     @Test
-    void addToBasketWorks(String bagelType) {
+    void addToBasketWorks() {
         Basket basket = new Basket();
+        List<String> bagelInventory = new ArrayList<>();
+        bagelInventory.add("plain");
+        bagelInventory.add("egg");
+        bagelInventory.add("blueberry");
+        bagelInventory.add("cinnamon");
+        bagelInventory.add("sourDough");
+        String bagelType = "sourDough";
 
+        boolean found = basket.search(bagelInventory, "sourDough");
+        Assertions.assertTrue(found);
     }
 
 }

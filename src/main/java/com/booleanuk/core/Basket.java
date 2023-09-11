b46a219eab615c0fe0fcc3538581a70be7ac12bc
Esther <estherbuijnsters@gmail.com>
2023-09-11 20:18:26 +0200

    commit new try at add bageltype test failing

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,23 +1,41 @@
 package com.booleanuk.core;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public class Basket {
+    List<String> BagelBasket = new ArrayList<>();
+    List<String> bagelInventory = new ArrayList<>();
+
+
+    public Basket() {
+        this.BagelBasket = new ArrayList<>();
+        this.bagelInventory = new ArrayList<>();
+        bagelInventory.add("plain");
+        bagelInventory.add("egg");
+        bagelInventory.add("blueberry");
+        bagelInventory.add("cinnamon");
+        bagelInventory.add("sourDough");
+
+    }
 
     public boolean search(List<String> bagelInventory, String bagelType) {
+        System.out.println(this.bagelInventory);
         return bagelInventory.contains(bagelType);
     }
 
-    public boolean addToBagelBasket(List<String> BagelBasket, String bagelType) {
-        if (!BagelBasket.contains(bagelType)) {
-            BagelBasket.add(bagelType);
-            return true;
-        }
-        return false;
+//    public boolean add(List<String> BagelBasket, String bagelType) {
+//        if (bagelInventory.contains(bagelType)) {
+//            this.BagelBasket.add(bagelType);
+//            System.out.println(BagelBasket);
+//            return true;
+//        }
+//        return false;
     }
-}
+
+
+
+
 
 
 
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class BasketTest {
 
     @Test
-    void addToBasketWorks() {
+    void searchForBagelType() {
         Basket basket = new Basket();
         List<String> bagelInventory = new ArrayList<>();
         bagelInventory.add("plain");
@@ -27,15 +27,26 @@ class BasketTest {
 
 //      This test passes because sourDough is in the bagelInventory
         boolean found = basket.search(bagelInventory, bagelType);
-        Assertions.assertTrue(found);
+        assertTrue(found);
 //      Another way to write this: Assertions.assertTrue(basket.search(bagelInventory, "sourDough"));
 
 //        these test pass because the outcome is indeed false, there are no bagels like this in the inventory
         Assertions.assertFalse(basket.search(bagelInventory, "chocolate"));
         Assertions.assertFalse(basket.search(bagelInventory, " "));
-
-//        This test if the bagel was added successfully
-        basket.addToBagelBasket(bagelInventory, bagelType);
-        Assertions.assertTrue(basket.addToBagelBasket(BagelBasket, bagelType));
     }
-}
+        @Test
+        void addBagelTypeWorks() {
+            Basket basket = new Basket();
+            List<String> bagelInventory = new ArrayList<>();
+            bagelInventory.add("plain");
+            bagelInventory.add("egg");
+            bagelInventory.add("blueberry");
+            bagelInventory.add("cinnamon");
+            bagelInventory.add("sourDough");
+            String bagelType = "sourDough";
+            List<String> BagelBasket = new ArrayList<>();
+
+            Assertions.assertTrue(basket.add(bagelType));
+        }
+    }
+

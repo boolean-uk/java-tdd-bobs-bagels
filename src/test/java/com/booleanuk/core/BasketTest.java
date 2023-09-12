df66b1007556be92459dcc10c830f16c3fae9a63
Esther <estherbuijnsters@gmail.com>
2023-09-12 11:55:34 +0200

    First Test

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -7,11 +7,32 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BasketTest {
 
+    @Test
 
+    public void testAddToBasketSuccessfull() {
+        Basket basket = new Basket(5); //object
+
+        assertTrue(basket.addToBasket("plain"));
+        assertEquals(1, basket.bagelBasket.size()); // check if it's added successfully
+        assertTrue(basket.bagelBasket.contains("plain")); // check if the correct bagel is in the basket
+
+    }
+
+    @Test
+
+    public void testAddToBasketFails() {
+        Basket basket = new Basket(1); //object
+
+        assertTrue(basket.addToBasket("plain"));
+        assertEquals(1, basket.bagelBasket.size()); // check if it's added successfully
+        assertTrue(basket.bagelBasket.contains("plain")); // check if the correct bagel is in the basket
+        assertFalse(basket.addToBasket("sourDough"));
+        assertEquals(1, basket.bagelBasket.size()); // check if it's still 1
+    }
 }
 
 

cb898e6f0f96c8529154972fb4e0b0a217f0822f
Esther <estherbuijnsters@gmail.com>
2023-09-12 12:10:48 +0200

    First Test passed

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -4,7 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Basket {
+ArrayList<String> bagelBasket;
 
+int bagelSize;
+
+    public Basket(int bagelSize) {
+        this.bagelSize = bagelSize;
+        this.bagelBasket = new ArrayList<>();
+    }
+
+    public boolean addToBasket(String bagelType) {
+        if (bagelBasket.size() >= this.bagelSize) {
+            System.out.println("Basket is full");
+            return false;
+        }
+        this.bagelBasket.add(bagelType);  //return this.bagelBasket.add(bagelType); same method
+        System.out.println("Basket is not full");
+        return true;
+    }
 }
 
 

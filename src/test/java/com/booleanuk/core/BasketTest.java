2384ed19a461342057de3ddccb946a8e0fd6097c
Giar Rehani <giarreh1@gmail.com>
2024-01-15 13:35:06 +0100

    Added test for Add bagel

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -5,4 +5,11 @@ import org.junit.jupiter.api.Test;
 
 class BasketTest {
 
+    @Test
+    public void testAddBagelToBasketReturnTrue(){
+        Basket basket = new Basket();
+        boolean result = basket.add("chocolate");
+        Assertions.assertTrue(result);
+    }
+
 }

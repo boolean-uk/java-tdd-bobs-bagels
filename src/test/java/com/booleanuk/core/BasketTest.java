0bbb120a881fcc7aa6b32408deae4d0cb7e0aec6
Petter Karstensen <petterkarstensen@gmail.com>
2024-01-15 13:34:43 +0100

    Add test for removeBagel

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -6,10 +6,23 @@ import org.junit.jupiter.api.Test;
 class BasketTest {
 
     @Test
-    public void testAddBagel(){
+    public void testAddBagelTrue(){
         Basket basket = new Basket();
-        Assertions.assertTrue(basket.addBagel("Sjoko"));
-        Assertions.assertFalse(basket.addBagel("Sjoko"));
+        Assertions.assertTrue(basket.addBagel("Chocolate"));
+    }
+    @Test
+    public void testAddBagelFalse(){
+        Basket basket = new Basket();
+        basket.addBagel("Chocolate");
+        Assertions.assertFalse(basket.addBagel("Chocolate"));
+    }
+
+    @Test
+    public void testRemoveBagel(){
+        Basket basket = new Basket();
+        basket.addBagel("Chocolate");
+        basket.addBagel("Strawberry");
+        Assertions.assertEquals("Bagel removed", basket.removeBagel);
     }
 
 }

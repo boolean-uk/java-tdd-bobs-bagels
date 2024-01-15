bc770e78cdb10e145203910424377dc30b0699ed
msg <mats.gravem@no.experis.com>
2024-01-15 13:32:39 +0100

    Pass isFull() tests

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -4,17 +4,26 @@ import java.util.ArrayList;
 
 public class Basket {
     private final ArrayList<String> basket;
+    private int capacity;
 
     public Basket() {
         this.basket = new ArrayList<>();
+        this.capacity = 5;
     }
 
     public boolean add(String bagel) {
+        if (this.isFull()) {
+            return false;
+        }
         return basket.add(bagel);
     }
 
     public boolean remove(String bagel) {
         return basket.remove(bagel);
+    }
+
+    public boolean isFull() {
+        return basket.size() >= capacity;
     }
 
     public ArrayList<String> getBasket() {

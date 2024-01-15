aacf83d73a93f0237b3d8d605e76905148f98230
Sandersaether <sander.sather@gmail.com>
2024-01-15 13:17:52 +0100

    2 tests added

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,13 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class Basket {
+    ArrayList<String> bagelBasket=new ArrayList<String>();
+
+
+    public String addBagel(String name){
+        return "";
+    }
 
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -5,4 +5,20 @@ import org.junit.jupiter.api.Test;
 
 class BasketTest {
 
+    @Test
+    public void checkIfBagelIsAdded(){
+        Basket basket =new Basket();
+        String bagelToAdd="Bagel";
+        basket.addBagel(bagelToAdd);
+        Assertions.assertTrue(basket.bagelBasket.contains(bagelToAdd));
+        Assertions.assertFalse(basket.bagelBasket.contains("Bagel2"));
+    }
+    @Test
+    public void addTwoOfTheSameBagel(){
+        Basket basket = new Basket();
+        Assertions.assertEquals("Bagel added", basket.addBagel("Bagel"));
+        Assertions.assertEquals("Bagel already exists in basket", basket.addBagel("Bagel"));
+
+    }
+
 }

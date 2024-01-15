17096a1ef229626b7b87333300fedaa44fd490e9
noahlenn <noahlenn@hotmail.com>
2024-01-15 13:21:38 +0100

    changed Basket

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,18 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class Basket {
+
+    public ArrayList<String> bagelsList = new ArrayList<>();
+
+    public Basket(){
+        this.bagelsList = new ArrayList<>();
+    }
+
+    public boolean add(String bagel){
+        return this.bagelsList.add(bagel);
+
+    }
 
 }

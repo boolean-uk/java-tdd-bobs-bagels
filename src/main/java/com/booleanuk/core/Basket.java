c5c10f4d2675ea990fb2ae70d1186d4b50a545e9
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-15 14:41:41 +0100

    updateCapacityToValidAmount test passed

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -39,7 +39,8 @@ public class Basket {
         return true;
     }
 
-    public void updateCapacity(int newCapacity) {
+    public boolean updateCapacity(int newCapacity) {
         capacity = newCapacity;
+        return true;
     }
 }

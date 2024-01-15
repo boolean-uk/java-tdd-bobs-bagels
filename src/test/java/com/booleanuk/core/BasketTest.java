26e78206f84aa1ebb431018520938fe99592280f
Axel Hansson <axel.co.hansson@gmail.com>
2024-01-15 16:09:00 +0100

    testSettingCapaityToLowerNumber changed to empty basket when changed to a lower number and returning messages

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -73,7 +73,7 @@ class BasketTest {
     public void testSettingCapaityToHigherNumber(){
         Basket basket = new Basket();
         Assertions.assertEquals(5, basket.maxCapacity);
-        basket.setBasketCapacity(10);
+        Assertions.assertEquals(("New max capaxity set to 10") , (basket.setBasketCapacity(10));
         Assertions.assertEquals(10, basket.maxCapacity);
     }
 
@@ -89,15 +89,11 @@ class BasketTest {
 
         Assertions.assertEquals(5, basket.maxCapacity);
 
-        basket.setBasketCapacity(3);
+        Assertions.assertEquals(("Basket is now empty and new max capaxity set to 3") , (basket.setBasketCapacity(3));
 
         Assertions.assertEquals(3, basket.maxCapacity);
+        Assertions.assertTrue(basket.items.isEmpty());
 
-        Assertions.assertTrue(basket.items.containsKey("Plain"));
-        Assertions.assertEquals(2, basket.items.get("Plain"));
-        Assertions.assertTrue(basket.items.containsKey("Salmon"));
-        Assertions.assertEquals(1, basket.items.get("Plain"));
-        Assertions.assertFalse(basket.items.containsKey("Sesame"));
 
     }
 

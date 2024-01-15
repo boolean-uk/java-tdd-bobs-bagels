2297fdb603362ab56d6b5b43b5011bfa4da5b197
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-15 13:17:07 +0100

    Create add method that pass user story 1 tests

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,17 @@
 package com.booleanuk.core;
 
-public class Basket {
+import java.util.HashMap;
 
+public class Basket {
+    HashMap<String, Integer> basketContents;
+
+    public Basket() {
+        this.basketContents = new HashMap<>();
+    }
+
+    public boolean add(String name) {
+        if (name.isBlank()) {return false;}
+        this.basketContents.put(name, 1);
+        return true;
+    }
 }

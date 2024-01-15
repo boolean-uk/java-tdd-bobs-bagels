2279ff53dd86ffcf72ccc547a081f22446cab1ca
Nikolaos Litsas <litsasnik@outlook.com>
2024-01-15 16:12:37 +0200

    Nikolaos Litsas

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,42 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
 public class Basket {
+public HashMap<String, Integer> bagelList;
+public int capacity;
 
+    public Basket() {
+        this.bagelList = new HashMap<>();
+        this.capacity = 3;
+    }
+
+    public boolean add(String item, int price) {
+
+        if (this.bagelList.containsKey(item)) {
+            return false;
+        }
+        this.bagelList.put(item, price);
+        return true;
+    }
+
+    public boolean remove(String item) {
+        if (this.bagelList.containsKey(item)) {
+            bagelList.remove(item);
+            return true;
+        } else {
+            System.out.println("This item is not in the basket");
+            return false;
+        }
+    }
+    public String capacity() {
+        if (bagelList.size() >= capacity) {
+            return "You have reached the capacity of the basket.";
+        }
+        return "you have space";
+    }
+
+    public int change(int newCap) {
+        this.capacity = newCap;
+        return this.capacity;
+    }
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -5,4 +5,37 @@ import org.junit.jupiter.api.Test;
 
 class BasketTest {
 
+    @Test
+    public void addingAnItemToBasketReturnTrue(){
+        Basket basket = new Basket();
+
+        Assertions.assertTrue(basket.add("bagel1", 4));
+    }
+
+    @Test
+    public void removingAnItemFromBasketReturnTrue(){
+        Basket basket = new Basket();
+        basket.add("bagel1", 4);
+        Assertions.assertTrue(basket.remove("bagel1"));
+    }
+
+    @Test
+    public void checkingTheCapacityReturnsTrue() {
+        Basket basket = new Basket();
+        basket.add("bagel1", 4);
+        basket.add("bagel2", 7);
+        basket.add("bagel3", 6);
+        basket.add("bagel4", 5);
+        Assertions.assertEquals("You have reached the capacity of the basket.", basket.capacity());
+    }
+
+    @Test
+    public void checkingChangedCapacity() {
+        Basket basket = new Basket();
+        basket.change(5);
+        Assertions.assertEquals(5, basket.change(5));
+    }
+    public void checkingMessage() {
+
+    }
 }

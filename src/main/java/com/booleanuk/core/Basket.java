c4c0b5c25b804400508af26a37e807abbea9d9c4
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-15 13:45:32 +0100

    addingBagelToFullBasket test passed

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,8 +1,23 @@
 package com.booleanuk.core;
 
-public class Basket {
+import java.util.HashMap;
 
+public class Basket {
+    public int capacity;
+    public int noOfItems;
+    public HashMap<String, Integer> items;
+
+    public Basket() {
+        capacity = 3;
+        noOfItems = 0;
+        items = new HashMap<>();
+    }
     public boolean add(String bagel) {
+        if(noOfItems == capacity) {
+            return false;
+        }
+        items.put(bagel, 1);
+        noOfItems++;
         return true;
     }
 }

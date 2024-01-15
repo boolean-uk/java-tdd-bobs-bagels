90391cd83ab683712de823e5f373d0e42eb1029f
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-01-15 14:22:30 +0100

    Implemented method addBagel in Basket

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -2,8 +2,14 @@ package com.booleanuk.core;
 
 public class Basket {
     Bagel[] basket = new Bagel[5];
+    int currentIndex = 0;
 
     public boolean addBagel(Bagel bagel) {
+        if(currentIndex == 4) {
+            return false;
+        }
+        basket[currentIndex] = bagel;
+        currentIndex++;
         return true;
     }
 }

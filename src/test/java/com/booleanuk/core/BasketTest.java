8a39d066c240af016457b9bb7827f0de4146b794
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-15 13:16:36 +0100

    Add test for addBagelBasket

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -5,4 +5,10 @@ import org.junit.jupiter.api.Test;
 
 class BasketTest {
 
+    @Test
+    public void testAddBagelToBasket() {
+        Basket basket = new Basket();
+
+        Assertions.assertTrue(basket.addBagelToBasket("Bagel1"));
+    }
 }

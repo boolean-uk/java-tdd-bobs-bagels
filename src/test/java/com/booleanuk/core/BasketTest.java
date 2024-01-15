91735d8c6785b114777a229b6798fad5a525be4e
msg <mats.gravem@no.experis.com>
2024-01-15 13:05:36 +0100

    Add add(bagel) test

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -3,6 +3,22 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class BasketTest {
+import java.util.ArrayList;
 
+class BasketTest {
+    private ArrayList<String> setupTestArray() {
+        ArrayList<String> testArray = new ArrayList<>();
+        testArray.add("Plain");
+        //testArray.add("Pumpernickel");
+        //testArray.add("Chocolate chip");
+        return testArray;
+    }
+
+    @Test
+    public void canAddBagelToBasket() {
+        Basket basket = new Basket();
+
+        Assertions.assertTrue(basket.add("Plain"));
+        Assertions.assertEquals(setupTestArray(), basket.getBasket());
+    }
 }

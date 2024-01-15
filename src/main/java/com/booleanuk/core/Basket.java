c8f3f363b983a6470579fa6abd52e645c95a2dfa
Axel Hansson <axel.co.hansson@gmail.com>
2024-01-15 14:30:53 +0100

    added getTotalAmount method and checks the amount before adding

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -10,12 +10,24 @@ public class Basket {
 
     public Basket(){
         this.items = new HashMap<>();
-        this.maxCapacity = 3;
+        this.maxCapacity = 5;
     }
 
     public String add(String bagel, int amount){
 
+        if(getTotalAmount() + amount > maxCapacity){
+            return "Basket is full";
+        }
+
         this.items.put(bagel, amount);
         return amount + " " +  bagel + " bagel added to basket";
+    }
+
+    public int getTotalAmount(){
+        int totalAmount = 0;
+        for (int amount : this.items.values()){
+            totalAmount += amount;
+        }
+        return totalAmount;
     }
 }

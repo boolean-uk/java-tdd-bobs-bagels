ed5efe056a98aeaad2ca271fc17d728277ef11c9
Marit <marit.moe2@ntebb.no>
2024-01-15 13:09:51 +0100

    Added simple code for adding bagel and returning true.

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,17 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class Basket {
+    ArrayList<String> items;
+
+    public Basket() {
+        this.items = new ArrayList<>();
+    }
+
+    public boolean add(String bagel) {
+        this.items.add(bagel);
+        return true;
+    }
 
 }

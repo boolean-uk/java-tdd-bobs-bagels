c62193f18760b4c49bf469598d0d1ab21b7a14fc
Giar Rehani <giarreh1@gmail.com>
2024-01-15 13:38:50 +0100

    Wrote code for the addBagel test

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,24 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class Basket {
+
+    ArrayList<String> basket;
+
+    public Basket(){
+        this.basket = new ArrayList<>();
+    }
+
+    public boolean add(String bagel){
+        if(bagel == null){
+            return false;
+        }
+        if(bagel.isEmpty()){
+            return false;
+        }
+        basket.add(bagel);
+        return true;
+    }
 
 }

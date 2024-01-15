4edcb92c69c290b0708820f30d0f685c422c1a85
noahlenn <noahlenn@hotmail.com>
2024-01-15 13:26:04 +0100

    changed BasketTest

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -6,13 +6,21 @@ import org.junit.jupiter.api.Test;
 class BasketTest {
 
     @Test
-    public void addItemToBasket(){
+    public void addItemToBasketReturnTrue(){
         Basket basket = new Basket();
 
         boolean result = basket.add("bread");
         Assertions.assertTrue(result);
+    }
 
 
+    @Test
+    public void removeItemFromBasket(){
+        Basket basket = new Basket();
+
+        basket.add("bread");
+        boolean result = basket.remove("bread");
+        Assertions.assertTrue(result);
     }
 
 }

4b7a84d5bbe7a992ae66c9e93186b13fff0a5473
Giar Rehani <giarreh1@gmail.com>
2024-01-15 13:56:18 +0100

    Added code for removeBagel tests

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -9,6 +9,16 @@ public class Basket {
     public Basket(){
         this.basket = new ArrayList<>();
     }
+    public boolean remove(String bagel) {
+        if(bagel == null){
+            return false;
+        }
+        if(!this.basket.contains(bagel)){
+            return false;
+        }
+        this.basket.remove((bagel));
+        return true;
+    }
 
     public boolean add(String bagel){
         if(bagel == null){

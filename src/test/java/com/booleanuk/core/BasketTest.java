3cabad5d2deff707dab41067f702900b3a15a1d2
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-15 13:25:49 +0100

    (changed name of test)

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 class BasketTest {
 
     @Test
-    public void addingBagelThatsNotInBasket() {
+    public void addingBagelToNotFullBasket() {
         Basket basket = new Basket();
         Assertions.assertTrue(basket.add("vanilla"));
     }

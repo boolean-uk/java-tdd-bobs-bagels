47bfa78de7041c40dac92a81a77063aab985679f
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-15 14:29:41 +0100

    Fix test null map check

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,10 +1,50 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Basket {
 
+    private Map<String, Map<String, Integer>> basket;
+    private Map<String, Integer> bagels;
+    private int capacityOfBasket;
 
+    public Basket(int capacityOfBasket, Map<String, Integer> bagels) {
+        this.capacityOfBasket = capacityOfBasket;
+        this.bagels = bagels;
+
+    }
     public boolean addBagelToBasket(String bagel1) {
 
-        return true;
+        if(this.basket != null) {
+
+            this.basket.put(bagel1, new HashMap<String, Integer>() {
+                {
+                    putIfAbsent("Quantity", 1);
+                    putIfAbsent("Price", bagels.get(bagel1));
+                }
+
+            });
+
+            return true;
+
+        } else if(bagels.containsKey(bagel1)){
+            Map<String, Map<String, Integer>> newBasket = new HashMap<>();
+            newBasket.put(bagel1, new HashMap<String, Integer>() {
+                {
+                    putIfAbsent("Quantity", 1);
+                    putIfAbsent("Price", bagels.get(bagel1));
+                }
+
+            });
+            this.basket = newBasket;
+            return true;
+        }
+
+
+
+
+        return false;
     }
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -3,12 +3,19 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 class BasketTest {
 
     @Test
     public void testAddBagelToBasket() {
-        Basket basket = new Basket();
+        Basket basket = new Basket(5, new HashMap<>() {
+            {
+                put("Bagel1", 15);
+            }
+        });
 
         Assertions.assertTrue(basket.addBagelToBasket("Bagel1"));
+
     }
 }

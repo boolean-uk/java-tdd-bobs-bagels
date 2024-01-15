4f1750f242e3825728969b9bd5e99d5e34e8784b
Alexander Niklasson <alexander.niklasson@live.com>
2024-01-15 13:00:21 +0100

    added first test for dm 4

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.Test;
 
 class BasketTest {
 
+    /* Domain model 1*/
     @Test
     public void testAddOneBagelToBasket(){
         Basket basket = new Basket();
         Assertions.assertEquals("Cream Cheese added", basket.addBagel("Cream Cheese"));
 
     }
-
     @Test
     public void testAddTwoOfTheSameBagelsToBasket(){
         Basket basket = new Basket();
@@ -19,7 +19,7 @@ class BasketTest {
         Assertions.assertEquals("Cream Cheese already exists in basket", basket.addBagel("Cream Cheese"));
 
     }
-
+    /* Domain model 2*/
     @Test
     public void testRemovingBagelThatExists(){
         Basket basket = new Basket();
@@ -34,6 +34,8 @@ class BasketTest {
         Assertions.assertEquals("Cream Cheese removed", basket.removeBagel("Cream Cheese"));
     }
 
+
+    /* Domain model 3*/
     @Test
     public void testAddToAFullBasket(){
         Basket basket = new Basket();
@@ -42,11 +44,17 @@ class BasketTest {
         basket.addBagel("Bacon");
         Assertions.assertEquals("Basket full", basket.addBagel("Mustard"));
     }
-
     @Test
     public void testAddBagelWhenNotAtCapacity(){
         Basket basket = new Basket();
         Assertions.assertEquals("Cream Cheese added", basket.addBagel("Cream Cheese"));
     }
 
+    /* Domain model 4*/
+    @Test
+    public void testReducingCapacityByTwoOfABasket(){
+        Basket basket = new Basket();
+        Assertions.assertTrue(true, basket.changeCapacity(2));
+
+    }
 }

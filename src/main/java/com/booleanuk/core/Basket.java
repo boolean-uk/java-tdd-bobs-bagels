9bc703e12ffcf026c44e965c0bd892f2aa23aab5
Knut S Rostad <knut.rostad@no.experis.com>
2024-01-15 13:45:18 +0100

    Added tests

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,8 @@
 package com.booleanuk.core;
 
+import java.lang.reflect.Array;
+
 public class Basket {
+
 
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -4,5 +4,46 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BasketTest {
+    @Test
+    public void testAddBagelToBasket(){
+        Basket basket = new Basket();
 
+        basket.addToBasket("Plain");
+        Assertions.assertEquals("Plain",basket.basketArr[basket.basketArr.length-1]);
+    }
+
+    @Test
+    public void testRemoveBagelFromBasket(){
+        Basket basket = new Basket();
+
+        basket.removeFromBasket("Plain");
+        Assertions.assertEquals("Chocolate",basket.basketArr[basket.basketArr.length-1]);
+    }
+
+    @Test
+    public void testAddToBasketDontExtendCapacity(){
+        Basket basket = new Basket();
+
+        basket.addToBasket("Plain");
+        basket.addToBasket("Vanilla");
+        basket.addToBasket("Strawberry");
+        boolean result = basket.addToBasket("Liquorice");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void testChangeArrayCapacity(){
+        Basket basket = new Basket();
+
+        basket.changeBasketCapacity(6);
+        Assertions.assertEquals(6,basket.basketArr.length);
+    }
+
+    @Test
+    public void testIfBagelExists(){
+        Basket basket = new Basket();
+
+        boolean result = basket.removeFromBasket("Liquorice");
+        Assertions.assertFalse(result);
+    }
 }

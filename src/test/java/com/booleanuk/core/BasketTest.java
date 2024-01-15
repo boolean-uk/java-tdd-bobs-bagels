599cb1a7c74c8454b74f253f342fdb59e577ea62
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-15 13:21:13 +0100

    addingBagelThatsNotInBasket test created

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -5,4 +5,9 @@ import org.junit.jupiter.api.Test;
 
 class BasketTest {
 
+    @Test
+    public void addingBagelThatsNotInBasket() {
+        Basket basket = new Basket();
+        Assertions.assertTrue(basket.add("vanilla"));
+    }
 }

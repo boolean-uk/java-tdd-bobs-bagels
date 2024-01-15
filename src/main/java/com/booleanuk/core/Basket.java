5b5c832bf6a4ed07c916ba51638386c13aeb9cdb
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-01-15 12:56:14 +0100

    Added test to see if the bagelBasket is empty

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -3,3 +3,21 @@ package com.booleanuk.core;
 public class Basket {
 
 }
+
+/*
+
+| Class  | Members           | Methods                            | Scenario               | Output/Results             |
+|--------|-------------------|------------------------------------|------------------------|----------------------------|
+| Basket | ArrayList<String> | String addBagel(String bagel)      | Adds a bagel into the  | String (bagel +" has been  |
+|        | bagelBasket       |                                    | bagel basket list      | added to you basket")      |
+|        |                   | String removeBagel(String bagel)   | Removes a bagel        | String (bagel+" has been   |
+|        |                   |                                    | from the basket list   | removed from your basket") |
+|        |                   | boolean basketIsFull(String bagel) | Tries to add a bagel   | if full return true        |
+|        |                   |                                    | but the basket is full | else return false          |
+|        |                   |                                    |                        |                            |
+|        |                   | ArrayList<String> changeCapacity() | Lets the manager       | return the new bagelBasket |
+|        |                   |                                    | change basket capacity | capacity                   |
+|        |                   | boolean itemDoesntExist(String     | Checks if the item     | if exist return true       |
+|        |                   | bagel)                             | removed exists         | else return false          |
+
+ */
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BasketTest {
-
-
+    @Test
+    public void testBasketIsEmpty(){
+        Basket basket = new Basket();
+        boolean isEmpty = basket.addBagel("");
+        Assertions.assertTrue(isEmpty);
+    }
 }

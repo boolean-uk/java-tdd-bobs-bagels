5315c394884c207ef623438de1267433e1e7daf9
hanna <hannaphamm1007@gmail.com>
2024-01-15 14:19:14 +0100

    add function 1

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,19 @@
 package com.booleanuk.core;
-
+import java.util.ArrayList;
 public class Basket {
+    ArrayList<String> bagelsList;
+    public Basket(){
+        this.bagelsList = new ArrayList<String>();
+
+
+    }
+    public boolean add(String bagels){
+        if(this.bagelsList.contains(bagels)){
+            return false;
+        }
+        this.bagelsList.add(bagels);
+        return true;
+
+    }
 
 }

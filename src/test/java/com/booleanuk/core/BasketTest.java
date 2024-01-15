719c714bef3cee960ca5ed70d992adabb3c8972d
Marit <marit.moe2@ntebb.no>
2024-01-15 13:03:20 +0100

    Added test for adding bagel.

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -4,5 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BasketTest {
+    @Test
+    public void testCanAddBagelToBasket() {
+        Basket basket = new Basket();
 
+        Assertions.assertTrue(basket.add(("Plain")));
+    }
 }

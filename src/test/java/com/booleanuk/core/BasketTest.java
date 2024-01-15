4a83ce0e0e5b74e85f3155f9bdfdbef5688798df
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-15 13:12:37 +0100

    Create tests for add user story 1

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -5,4 +5,15 @@ import org.junit.jupiter.api.Test;
 
 class BasketTest {
 
+    @Test
+    public void testAddBagelToEmptyBasket() {
+        Basket basket = new Basket();
+        Assertions.assertTrue(basket.add("Brown"));
+    }
+
+    @Test
+    public void testAddBagelWithoutNameBasket() {
+        Basket basket = new Basket();
+        Assertions.assertFalse(basket.add(""));
+    }
 }

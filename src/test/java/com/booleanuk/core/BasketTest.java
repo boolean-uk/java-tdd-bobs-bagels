302a9f6f7e0f4cdb6410666c0ce1fbe2bcdcfc05
Maha Riazi <maha.riazi@hotmail.no>
2024-01-15 13:04:28 +0100

    Red: test 2 added

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -9,12 +9,28 @@ class BasketTest {
         Basket basket = new Basket();
         VeganBagel vegan = new VeganBagel();
         basket.add(vegan);
+
         Assertions.assertTrue(basket.itemList.containsKey(vegan));
     }
 
     @Test
     public void removeBagelFromCart(){
+        Basket basket = new Basket();
+        SpicyBagel spicy = new SpicyBagel();
+        basket.add(spicy);
+        basket.remove(spicy);
 
+        Assertions.assertTrue(basket.itemList.isEmpty());
+
+        VeganBagel vegan1 = new VeganBagel();
+        VeganBagel vegan2 = new VeganBagel();
+        VeganBagel vegan3 = new VeganBagel();
+        basket.add(vegan1);
+        basket.add(vegan2);
+        basket.add(vegan3);
+        basket.remove(vegan2);
+
+        Assertions.assertFalse(basket.itemList.containsKey(vegan2));
     }
 
     @Test

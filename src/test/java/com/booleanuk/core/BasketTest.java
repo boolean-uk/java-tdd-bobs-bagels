6a5ba9296bca30bc3afc7db427bfc1a14235daa8
Robin Kaastrup <robin.kaastrup@no.experis.com>
2024-01-15 13:10:49 +0100

    Added test cases for bagel removal

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -13,4 +13,20 @@ class BasketTest {
         Assertions.assertTrue(basket.addBagel("With Everything"));
         Assertions.assertTrue(basket.addBagel("Sesame"));
     }
+
+    @Test
+    public void testRemoveBagelInBasket() {
+        Basket basket = new Basket();
+
+        basket.addBagel("Cream Cheese");
+        Assertions.assertTrue(basket.removeBagel("Cream Cheese"));
+    }
+
+    @Test
+    public void testRemoveBagelNotInBasket() {
+        Basket basket = new Basket();
+
+        Assertions.assertFalse(basket.removeBagel("Cream Cheese"));
+    }
 }
+

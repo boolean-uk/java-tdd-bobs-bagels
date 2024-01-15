71d5d3117d915c687ff88dbb4e152abca57a2550
stp <steventphung6@gmail.com>
2024-01-15 14:00:45 +0100

    Add and remove basketTest

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -3,6 +3,34 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 class BasketTest {
+    @Test
+    public void checkAddedBagel() {
+        Basket basket = new Basket();
+        String bagelOne = "Plain";
+
+        basket.list.add(bagelOne);
+
+        Assertions.assertTrue(basket.list.contains(bagelOne));
+    }
+
+    @Test
+    public void removeBagelFromBasket() {
+        Basket basket = new Basket();
+
+        String bagelOne = "Plain";
+
+        //Checks if the bagel was added to be removed later
+        basket.list.add(bagelOne);
+        Assertions.assertTrue(basket.list.contains(bagelOne));
+
+        basket.list.remove(bagelOne);
+        Assertions.assertFalse(basket.list.contains(bagelOne));
+
+    }
+
+
 
 }

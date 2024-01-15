a928dd29248802572d8fb32a07e4b1673dd35e6c
Axel Hansson <axel.co.hansson@gmail.com>
2024-01-15 14:48:46 +0100

    fixed typos in test

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -7,8 +7,8 @@ class BasketTest {
     @Test
     public void testIfAddAddsBagelsProperly(){
         Basket basket = new Basket();
-        Assertions.assertEquals(("1 Plain bagel added to basket"), basket.add("Plain", 1));
-        Assertions.assertEquals(("3 Sesame bagel added to basket"),basket.add("Sesame", 3));
+        Assertions.assertEquals(("1 Plain bagel(s) added to basket"), basket.add("Plain", 1));
+        Assertions.assertEquals(("3 Sesame bagel(s) added to basket"),basket.add("Sesame", 3));
         Assertions.assertTrue(basket.items.containsKey("Plain"));
         Assertions.assertEquals(basket.items.get("Plain"), 1);
         Assertions.assertTrue(basket.items.containsKey("Sesame"));
@@ -19,8 +19,8 @@ class BasketTest {
     public void testAddingWhenBasketIsFull(){
 
         Basket basket = new Basket();
-        Assertions.assertEquals(("4 Plain bagel added to basket"), basket.add("Plain", 4));
-        Assertions.assertEquals(("1 Sesame bagel added to basket"), basket.add("Sesame", 1));
+        Assertions.assertEquals(("4 Plain bagel(s) added to basket"), basket.add("Plain", 4));
+        Assertions.assertEquals(("1 Sesame bagel(s) added to basket"), basket.add("Sesame", 1));
         Assertions.assertEquals(("Basket is full"), basket.add("Sesame", 1));
 
     }

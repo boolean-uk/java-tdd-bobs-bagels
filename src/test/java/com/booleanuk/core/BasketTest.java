a46ff00c5dc2cec556105e7097e11ed2896f6c48
Petros Mylonas <petrosmylonascs@gmail.com>
2024-01-15 16:21:18 +0200

    Petros Mylonas

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -25,5 +25,36 @@ class BasketTest {
         assertEquals(0, basket.getBasket().size());
         assertFalse(basket.getBasket().contains(bagel));
     }
+    @Test
+    public void testRemoveNonexistentBagelFromBasket() {
+        Basket basket = new Basket();
+        String bagelInBasket = "Sesame";
+        String bagelNotInBasket = "Blueberry";
+
+        basket.addBagel(bagelInBasket);
+        basket.removeBagel(bagelNotInBasket);
+
+        assertEquals(1, basket.getBasket().size());
+        assertTrue(basket.getBasket().contains(bagelInBasket));
+        assertFalse(basket.getBasket().contains(bagelNotInBasket));
+    }
+    @Test
+    public void testChangeBasketCapacity() {
+        Basket basket = new Basket();
+        String bagel1 = "Cinnamon Raisin";
+        String bagel2 = "Poppy Seed";
+
+        basket.addBagel(bagel1);
+
+        assertEquals(5, basket.getCapacity());
+
+        basket.setCapacity(10);
+
+        assertEquals(10, basket.getCapacity());
+
+        basket.addBagel(bagel2);
+        assertEquals(2, basket.getBasket().size());
+        assertTrue(basket.isBasketFull());
+    }
 }
 

fa81d217ffbdd6a7792ac6bd72664506c04e0e77
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-15 13:27:55 +0100

    Add baskeLimit to Basket so that tests for user story 3 pass

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -4,13 +4,15 @@ import java.util.HashMap;
 
 public class Basket {
     HashMap<String, Integer> basketContents;
+    int basketLimit;
 
     public Basket() {
         this.basketContents = new HashMap<>();
+        this.basketLimit = 3;
     }
 
     public boolean add(String name) {
-        if (name.isBlank()) {return false;}
+        if (name.isBlank() || this.basketContents.size() >= basketLimit) {return false;}
         this.basketContents.put(name, 1);
         return true;
     }

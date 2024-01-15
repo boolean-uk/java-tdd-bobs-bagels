0e0206e5f198751e31fcba5d93563672b6162ca8
Osamah Al-maliki <osamah.al-maliki@no.experis.com>
2024-01-15 13:48:53 +0100

    Added first test

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -4,5 +4,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BasketTest {
+    @Test
+    public void addingBagelToBasketReturnsTrue() {
+        Basket basket = new Basket();
+
+        String bagel = basket.add("Plain bagel");
+
+        Assertions.assertEquals("Plain bagel", bagel);
+    }
+
 
 }

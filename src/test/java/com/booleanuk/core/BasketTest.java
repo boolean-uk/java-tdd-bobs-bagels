2608619dced3243500b318d84fbc1de6b63ed5b4
Hiko Yüksek <33344671+hkyksk@users.noreply.github.com>
2024-01-16 18:17:49 +0100

    Red: Added test

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.Test;
 class BasketTest {
     @Test
     public void addBagelToBasket(){
-
+        Basket basket = new Basket();
+        Bagel sesame = new Bagel("Sesame Bagel", 4.50);
+        basket.add(sesame);
+        Assertions.assertEquals(basket.items.get(sesame).name, "Sesame Bagel");
+        Assertions.assertEquals(basket.items.get(sesame).price, 4.50);
     }
     @Test
     public void checkIfBasketIsFull(){
-
     }
     @Test
     public void removeBagelFromBasket(){

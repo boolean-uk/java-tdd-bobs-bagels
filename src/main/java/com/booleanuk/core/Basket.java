2fd446923ae86a7002bc29543bf03596371d00ef
Mads Gjengaar <mig.gjengaar@gmail.com>
2024-01-16 11:35:09 +0100

    Added Tests

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,24 @@
 package com.booleanuk.core;
 
-public class Basket {
+import java.util.ArrayList;
 
+public class Basket {
+    private int maxCapcity = 5;
+    private final ArrayList<String> items = new ArrayList<>();
+
+    public boolean add(String bagelName) {
+        return true;
+    }
+
+    public boolean remove(String bagelName) {
+        return true;
+    }
+
+    public void changeBasketCapacity(int capacity) {
+
+    }
+
+    public int getCapacity() {
+        return maxCapcity;
+    }
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -3,6 +3,40 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class BasketTest {
+public class BasketTest {
+    @Test
+    public void testAdding() {
+        Basket _basket = new Basket();
 
+        Assertions.assertTrue(_basket.add("plain_bagel"));
+        Assertions.assertTrue(_basket.add("everything_bagel"));
+        Assertions.assertTrue(_basket.add("egg_bagel"));
+
+        Assertions.assertFalse(_basket.add("everything_bagel"));
+    }
+
+    @Test
+    public void testRemoving() {
+        Basket _basket = new Basket();
+
+        _basket.add("plain_bagel");
+        _basket.add("everything_bagel");
+        _basket.add("egg_bagel");
+
+        Assertions.assertTrue(_basket.remove("everything_bagel"));
+        Assertions.assertFalse(_basket.remove("gluck_bagel"));
+    }
+
+    @Test
+    public void adjustBasketCapacity() {
+        Basket _basket = new Basket();
+
+        _basket.changeBasketCapacity(3);
+
+        Assertions.assertEquals(3, _basket.getCapacity());
+
+        _basket.changeBasketCapacity(10);
+
+        Assertions.assertNotEquals(3, _basket.getCapacity());
+    }
 }

002cdf54e2c8295756abff550d035999f8ba0e59
Hiko Yüksek <33344671+hkyksk@users.noreply.github.com>
2024-01-16 19:26:23 +0100

    Green: Minimum code to pass

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -16,7 +16,6 @@ class BasketTest {
     @Test
     public void checkIfBasketIsFull(){
         Basket basket = new Basket();
-        int initialCapacity = 0;
         String sesame = "Sesame Bagel";
         double price = 4.50;
         basket.add(sesame, price);

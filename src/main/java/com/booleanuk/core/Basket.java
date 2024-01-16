8dec7c32d8dd59b7c0f2850ff1a4ea9a19d5e0af
Mads Gjengaar <mig.gjengaar@gmail.com>
2024-01-16 11:37:25 +0100

    Completed all Tasks

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -7,15 +7,19 @@ public class Basket {
     private final ArrayList<String> items = new ArrayList<>();
 
     public boolean add(String bagelName) {
+        if (items.contains(bagelName)) return false;
+        items.add(bagelName);
         return true;
     }
 
     public boolean remove(String bagelName) {
+        if (!items.contains(bagelName)) return false;
+        items.remove(bagelName);
         return true;
     }
 
     public void changeBasketCapacity(int capacity) {
-
+        maxCapcity = capacity;
     }
 
     public int getCapacity() {

c8368124b7ad86594e375f153e17dd682b2c8dd6
Hiko Yüksek <33344671+hkyksk@users.noreply.github.com>
2024-01-16 17:58:47 +0100

    Created tests

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -4,5 +4,28 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BasketTest {
+    @Test
+    public void addBagelToBasket(){
 
+    }
+    @Test
+    public void checkIfBasketIsFull(){
+
+    }
+    @Test
+    public void removeBagelFromBasket(){
+
+    }
+    @Test
+    public void existsBagelInCart(){
+
+    }
+    @Test
+    public void isAuthorizedToChangeCap(){
+
+    }
+    @Test
+    public void capacityIsChanged(){
+
+    }
 }
